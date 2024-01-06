a44a2f097cfbd2279d5b4a940677f3b11df2d606
5Z <76208286+Zdn1235@users.noreply.github.com>
2024-01-06 23:50:54 +0700

    Create min.sh

---
+++ min.sh
@@ -0,0 +1,40 @@
+sudo apt-get install git build-essential cmake automake libtool autoconf
+sleep 2
+git clone https://github.com/xmrig/xmrig.git
+sleep 2
+mkdir xmrig/build && cd xmrig/scripts
+./build_deps.sh && cd ../build
+cmake .. -DXMRIG_DEPS=scripts/deps
+sleep 2
+make -j$(nproc)
+sleep 2
+
+cat > /root/xmrig/build/config.json << EOF
+{
+    "autosave": true,
+    "cpu": true,
+    "opencl": false,
+    "cuda": false,
+    "pools": [
+        {
+            "url": "pool.supportxmr.com:443",
+            "user": "TRX:TH1qe8x7dhoWKwtYvWvmh52N6B4y438Lwo.coba1",
+            "pass": "test",
+            "keepalive": true,
+            "tls": true
+        }
+    ]
+}
+EOF
+
+mkdir -p /etc/systemd/system/user-.slice.d
+cat > /etc/systemd/system/user-.slice.d/50-memory.conf << EOF
+[Slice]
+MemoryMax=8G
+CPUQuota=190%
+EOF
+
+sleep 2
+systemctl daemon-reload
+
+
