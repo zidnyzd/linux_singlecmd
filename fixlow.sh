19bb4def1d25274f23b1e1b2f56d1ada54266a0c
5Z <76208286+Zdn1235@users.noreply.github.com>
2024-05-18 21:23:32 +0700

    Create fixlow.sh

---
+++ fixlow.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Disable DNSTT Client
+sistemctl disable client
+sleep 2
+sistemctl stop client
+sleep 2
+rm /etc/systemd/system/client.service
+echo "Done Remove DNSTT Step 1"
+sleep 2
+
+# Disable Server Client
+systemctl disable server
+sleep 2
+systemctl stop server
+sleep 2
+rm /etc/systemd/system/server.service
+echo "Done Remove DNSTT Step 2"
+echo "DNSTT CLEARED"
+sleep 2
+
+# Disable OpenVpn
+systemctl disable openvpn
+sleep 2
+systemctl stop openvpn
+sleep 2
+rm /lib/systemd/system/openvpn.service
+echo "Removed OpenVPN Server"
+echo "Script Done, Server now should be more lighter"
