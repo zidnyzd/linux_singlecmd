6b9f9908a7e0ccb384c339a76fb8335aacbdb98d
5Z <76208286+Zdn1235@users.noreply.github.com>
2024-07-16 18:15:20 +0700

    debian11

---
+++ fixdeb11.sh
@@ -0,0 +1,16 @@
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y 
+sudo apt-get install build-essential -y
+sudo apt-get install python-dev -y
+sudo apt-get install python-setuptools -y
+sudo apt-get install libncursesw5-dev -y
+sudo apt-get install python-pip -y
+sudo apt-get install python-smbus -y 
+sudo apt-get install libgdbm-dev -y
+sudo apt-get install libc6-dev -y 
+sudo apt-get install zlib1g-dev -y
+sudo apt-get install libsqlite3-dev -y
+sudo apt-get install tk-dev -y 
+sudo apt-get install openssl -y 
+sudo apt-get install libffi-dev -y
