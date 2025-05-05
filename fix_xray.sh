dad95c412f860949e0918261a1b60f3a80536fae
Muhammad Zidny Ilhami <zidnyilhami01@gmail.com>
2025-05-05 11:23:08 +0700

    xray

--- fix_xray.sh
+++ fix_xray.sh
@@ -1,30 +1,8 @@
-#!/bin/bash
-
-# Path ke file config.json
-VMESS_CONFIG="/etc/xray/vmess/config.json"
-VLESS_CONFIG="/etc/xray/vless/config.json"
-TROJAN_CONFIG="/etc/xray/trojan/config.json"
-
-# Fungsi untuk mengubah sniffing enabled dari true menjadi false
-ubah_sniffing() {
-    local config_file=$1
-    if [[ -f "$config_file" ]]; then
-        sed -i 's/"enabled": true/"enabled": false/g' "$config_file"
-        echo "Fixing XRAY $config_file."
-    else
-        echo "File $config_file tidak ditemukan."
-    fi
-}
-
-# Mengubah sniffing pada vmess/config.json
-ubah_sniffing "$VMESS_CONFIG"
-
-# Mengubah sniffing pada vless/config.json
-ubah_sniffing "$VLESS_CONFIG"
-
-# Mengubah sniffing pada trojan/config.json
-ubah_sniffing "$TROJAN_CONFIG"
-
-systemctl restart vmess@config
-systemctl restart vless@config
-systemctl restart trojan@config
+sudo mv /usr/local/bin/xray /usr/local/bin/xray.bak && \
+    wget -qO /usr/local/bin/xray "https://raw.githubusercontent.com/zidnyzd/linux/main/xray/xray.linux.64bit"  && \
+chmod +x /usr/local/bin/xray && \
+sed -i 's/"enabled": true/"enabled": false/' /etc/xray/vmess/config.json \
+    /etc/xray/vless/config.json \
+    /etc/xray/trojan/config.json \
+    /etc/xray/shadowsocks/config.json && \
+systemctl restart vmess@config vless@config trojan@config shadowsocks@config
