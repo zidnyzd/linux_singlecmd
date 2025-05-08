2fddf3d9ec4c4dd7b67f77337c5aebe850d9c96e
Muhammad Zidny Ilhami <zidnyilhami01@gmail.com>
2025-05-08 17:40:47 +0700

    Update banner.sh

--- banner.sh
+++ banner.sh
@@ -5,31 +5,37 @@ rm -f /etc/fightertunnel.txt
 
 # Buat ulang dengan konten baru
 cat << 'EOF' > /etc/fightertunnel.txt
-<pre>
-<h2 style="text-align:center">
-<font color='#ff0000'>Z</font><font color='#ff0000'>I</font><font color='#ff0000'>D</font><font color='#FFFFFF'>S</font><font color='#FFFFFF'>T</font><font color='#FFFFFF'>O</font><font color='#FFFFFF'>R</font><font color='#FFFFFF'>E</font>
+<h2 style="text-align:center;">
+  <font color="#ff0000">Z</font>
+  <font color="#ff0000">I</font>
+  <font color="#ff0000">D</font>
+  <font color="#FFFFFF">S</font>
+  <font color="#FFFFFF">T</font>
+  <font color="#FFFFFF">O</font>
+  <font color="#FFFFFF">R</font>
+  <font color="#FFFFFF">E</font>
 </h2>
-
-<font color="white">⚡ PREMIUM VPN & SSH SERVICE ⚡</font>
-
-<font color="yellow">📋 SERVER RULES:</font>
-<font color="white">❌ No DDOS/Torrent/Multi Login/Sharing</font>
-<font color="white">🔄 Auto Reboot: 02:00 AM GMT+7</font>
-
-<font color="cyan">📱 CONTACT US:</font>
-<font color="white">📲 WA: https://wa.me/+6285184673439</font>
-<font color="white">📨 TG: https://t.me/storezid2</font>
-<font color="white">📢 CH: https://t.me/zidstorevpn</font>
-
-<font color="green">🤖 BOT AUTO ORDER:</font>
-<font color="white">🔗 https://t.me/zidvpnstorebot</font>
-<font color="white">⏰ 24/7 Service Available</font>
-
-<font color="yellow">🛍️ OUR SERVICES:</font>
-<font color="white">🔹 VPS UB: http://t.me/zidstorevpn/16</font>
-<font color="white">🔹 VPS DO: http://t.me/zidstorevpn/17</font>
-<font color="white">🔹 XL: http://t.me/zidstorevpn/19</font>
-</pre>
+<br>
+<h3 style="text-align:center;"><font color="white">Server Rules :</font></h3>
+<div style="text-align:center;">
+  <font color="white">No Torrent<br>
+  No MultiLogin<br>
+  No Reshare Account<br>
+  Melanggar? Auto Delete Account<br><br>
+  <b>CONTACT US:</b><br>
+  Whatsapp: https://wa.me/+6285184673439<br>
+  Telegram: https://t.me/storezid2<br>
+  Telegram Channel: https://t.me/zidstorevpn<br><br>
+  Bot Auto Order VPN/SSH<br>
+  https://t.me/zidvpnstorebot<br>
+  Buat Akun Kapan Saja, Online 24/7<br>
+  <br>
+  <b>OUR SERVICES:</b><br>
+  VPS Unlimited Bandwidth: https://t.me/zidstorevpn/16<br>
+  VPS Digital Ocean: https://t.me/zidstorevpn/17<br>
+  Tembak Paket XL UTS, XCV, XCP, Akrab: https://t.me/zidstorevpn/19<br>
+  </font>
+</div>
 EOF
 
 # Set permission agar bisa dibaca
