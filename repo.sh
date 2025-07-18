cd3fc964802ebf40530ece61c6707cf78e3a3d8d
Muhammad Zidny Ilhami <zidnyilhami01@gmail.com>
2025-07-18 15:37:12 +0700

    Update repo.sh

--- repo.sh
+++ repo.sh
@@ -13,10 +13,12 @@ write_default_repos() {
         case "$VERSION" in
             buster)
                 cat <<EOF > /etc/apt/sources.list
-deb http://deb.debian.org/debian buster main contrib non-free
-deb http://deb.debian.org/debian buster-updates main contrib non-free
-deb http://security.debian.org/debian-security/ buster/updates main contrib non-free
+deb http://archive.debian.org/debian/ buster main contrib non-free
+deb http://archive.debian.org/debian/ buster-updates main contrib non-free
+deb http://archive.debian.org/debian-security/ buster/updates main contrib non-free
 EOF
+                # Disable Check-Valid-Until for EOL repos
+                echo 'Acquire::Check-Valid-Until "false";' > /etc/apt/apt.conf.d/99no-check-valid-until
                 ;;
             bullseye)
                 cat <<EOF > /etc/apt/sources.list
