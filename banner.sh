187885b17d08a8b068f619d60c15259235f5457a
Muhammad Zidny Ilhami <zidnyilhami01@gmail.com>
2026-04-03 14:13:08 +0700

    Update banner.sh

--- banner.sh
+++ banner.sh
@@ -22,24 +22,22 @@ cat << 'EOF' > /etc/fightertunnel.txt
   No MultiLogin<br>
   No Reshare Account<br>
   Melanggar? Delete Account<br><br>
-  Bot Auto Order UDP ZIVPN, SSH WS, VMess, VLESS, Trojan.<br>
-  https://t.me/zidvpnstorebot<br>
-  Buat Akun Kapan Saja, Pilih hari sesuai keinginanmu (Khusus SSH, VMess, VLESS, Trojan)<br>
+  Web Auto Order UDP ZIVPN, SSH WS, VMess, VLESS, Trojan. dan juga Tembak Paket Data dalam satu Website<br>
+  <br>
+  https://panel.zidstore.net<br>
   <br> 
-  BOT Online 24/7<br>
+  Bisa order kapan saja 24/7. Top Up Instan tanpa biaya admin<br>
   <br>
-  Tembak Paket Data di Web <br>https://panel.zidstore.net<br>
-  Top Up Instant 24/7<br>
-  <br>
-  Paket XL yang Tersedia di Web :<br>
-  XL Edukasi 15GB, 8K-an!, XL Conference 15GB, 11K-an!, XL Akrab Kuota s/d 163GB dan Bisa untuk AXIS, XL Circle Kuota s/d 84GB, XL Xtra Combo Plus 5GB / XCP 0KB + Bonus Kuota / Addon 10-15GB, Masa Aktif All Operator, AXIS Data, By.U Data, TRI & INDOSAT Data & HiFi, Kuota s/d 500GB, dan banyak lagi<br>
+  Paket Data yang Tersedia di Web :<br>
+  XL Edukasi 15GB hanya 8K-an!, XL Conference 15GB hanya 11K-an!, XL Akrab Kuota s/d 163GB dan Bisa untuk AXIS, XL Circle Kuota s/d 84GB, XL Xtra Combo Plus 5GB / XCP 0KB + Bonus Kuota / Addon XCP 10-15GB, Masa Aktif All Operator, AXIS Data, By.U Data, TRI & INDOSAT Data & HiFi, Kuota s/d 500GB, dan banyak lagi<br>
   <br>
   <b>OUR SERVICES:</b><br>
   VPS Unlimited Bandwidth: https://t.me/zidstorevpn/16<br>
   <br><b>CONTACT US:</b><br>
   Telegram: https://t.me/storezid2<br>
   Telegram Channel: https://t.me/zidstorevpn<br>
-  Whatsapp: https://wa.me/+6285184673439<br><br>
+  Whatsapp: https://wa.me/+6285184673439<br>
+  Whatsapp Channel: https://whatsapp.com/channel/0029Vb7SCT4A2pLI7JkFKN2w<br><br>
   </font>
 </div>
 EOF
