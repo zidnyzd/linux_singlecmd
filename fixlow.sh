719e1fcec7656ebe6a7330c9396a294c09e18781
Muhammad Zidny Ilhami <zidnyilhami01@gmail.com>
2026-04-18 08:41:25 +0700

    Update fixlow.sh

--- fixlow.sh
+++ fixlow.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # Disable DNSTT Client
-systemctl disable client
+systemctl disable dnstt-client
 sleep 2
 systemctl stop client
 sleep 2
-rm /etc/systemd/system/client.service
+rm /etc/systemd/system/dnstt-client.service
 echo "Done Remove DNSTT Step 1"
 sleep 2
 
 # Disable Server Client
-systemctl disable server
+systemctl disable dnstt-server
 sleep 2
-systemctl stop server
+systemctl stop dnstt-server
 sleep 2
-rm /etc/systemd/system/server.service
+rm /etc/systemd/system/dnstt-server.service
 echo "Done Remove DNSTT Step 2"
 echo "DNSTT CLEARED"
 sleep 2
@@ -24,5 +24,13 @@ rm /etc/cron.d/daily_reboot
 sleep 2
 systemctl restart cron
 echo "Done Remove Auto Reboot"
+
+# fix kernel
+mv /var/lib/dpkg/info/linux-image-unsigned-7.0.0-070000-generic.postrm \
+   /var/lib/dpkg/info/linux-image-unsigned-7.0.0-070000-generic.postrm.bak
+sleep 2
+dpkg --remove --force-remove-reinstreq linux-image-unsigned-7.0.0-070000-generic
+echo "Done Remove Kernel 7.0.0-070000-generic"
+sleep 2
 
 echo "Script Done, Server now should be more lighter"
