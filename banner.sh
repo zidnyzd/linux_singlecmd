0280abf088c7fe7aefd08693a9a0db93f5d324ba
Muhammad Zidny Ilhami <zidnyilhami01@gmail.com>
2026-06-15 17:07:25 +0700

    Update banner.sh

--- banner.sh
+++ banner.sh
@@ -6,15 +6,17 @@ rm -f /etc/fightertunnel.txt
 # Buat ulang dengan konten baru
 cat << 'EOF' > /etc/fightertunnel.txt
 <p align="center">
-  <font color="#3399FF" size="5"><b>ZIDSTORE</b></font><br>
+  <font color="#3399FF" size="5"><b>ZIDSTORE.NET</b></font><br>
   <font color="#ffffff">──────────────────────────────────────────</font><br>
-  <font color="#3399FF"><b>🛍️ LAYANAN TERSEDIA DI WEBSITE</b></font><br>
+  <font color="#3399FF"><b>🛍️ LAYANAN YANG TERSEDIA DI WEBSITE</b></font><br>
   <font color="#FFFF00"><b>https://zidstore.net</b></font><br>
   <font color="#ffffff">
-    • Web Auto Order SSH WS, VMess, VLESS, Trojan, Pulsa, & Paket Data.<br>
-    • Login Instan Google | Topup Otomatis 24/7 Biaya Admin Rp.0.<br>
-    • Pilih Paket - SCAN QRIS - Paket Masuk Otomatis.<br>
-    • Tanpa Perlu Topup Saldo Web.<br>
+    • Order Pulsa & Paket Data ALL Operator<br>
+    • Order SSH WS, VMess, VLESS, Trojan<br>
+    • Daftar dan Login Instan dengan Google 1x Klik<br>
+    • QRIS Tanpa Biaya Admin Rp.0 fee<br>
+    • Pilih Paket - SCAN QRIS - Paket Masuk Otomatis<br>
+    • Tanpa Perlu Topup Saldo Web<br>
     <br>
     <b>📦 PAKET DATA POPULER:</b><br>
     XL Edukasi 15GB (8K-an!), XL Conference 15GB (11K-an!), XL Xtra Kuota / iFlix 14GB (12K-an!),<br>
